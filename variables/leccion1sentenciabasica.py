49e6327da25ac07817ae767fccd5e5d69cb28389
leo57munoz <leo57munoz@gmail.com>
2022-06-20 20:47:32 -0600

    Clase 1 de Python

---
+++ variables/leccion1sentenciabasica.py
@@ -0,0 +1,19 @@
+print('Hola mundo')
+#Comentario
+
+#Variables
+camisa=1
+print(camisa)
+print(type(camisa))
+
+blusa="rosada"
+print(type(blusa))
+
+camisa=2
+print(camisa)
+
+dato=input("Ingrese un dato")
+print(dato)
+
+dato2= int (input("Ingrese un numero entero "))
+print(dato2)
